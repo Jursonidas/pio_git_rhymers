79697e844b4fb29a506ef1b30fd420cde773122c
251161 <251161@edu.p.lodz.pl>
2025-03-15 16:51:09 +0100

    Poprawienie nazw pol klas total na _total

--- src/main/java/edu/kis/vh/nursery/defaultCountingOutRhymer.java
+++ src/main/java/edu/kis/vh/nursery/defaultCountingOutRhymer.java
@@ -4,31 +4,31 @@ public class defaultCountingOutRhymer {
 
     private int[] NUMBERS = new int[12];
 
-    public int total = -1;
+    public int _total = -1;
 
     public void countIn(int in) {
         if (!isFull())
-            NUMBERS[++total] = in;
+            NUMBERS[++_total] = in;
     }
 
     public boolean callCheck() {
-        return total == -1;
+        return _total == -1;
     }
 
     public boolean isFull() {
-        return total == 11;
+        return _total == 11;
     }
 
     protected int peekaboo() {
         if (callCheck())
             return -1;
-        return NUMBERS[total];
+        return NUMBERS[_total];
     }
 
     public int countOut() {
         if (callCheck())
             return -1;
-        return NUMBERS[total--];
+        return NUMBERS[_total--];
     }
 
 }
