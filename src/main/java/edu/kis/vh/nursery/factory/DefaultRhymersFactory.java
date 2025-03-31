b25e4918b347a8b8c114ef78531d017476c4acf5
Jurus <jursmarcel555@gmail.com>
2025-03-31 13:15:07 +0200

    17. Dodano komentarze JavaDoc do pliku DefaultRhymersFactory

--- src/main/java/edu/kis/vh/nursery/factory/DefaultRhymersFactory.java
+++ src/main/java/edu/kis/vh/nursery/factory/DefaultRhymersFactory.java
@@ -4,23 +4,49 @@ import edu.kis.vh.nursery.DefaultCountingOutRhymer;
 import edu.kis.vh.nursery.FIFORhymer;
 import edu.kis.vh.nursery.HanoiRhymer;
 
+/**
+ * Klasa fabryczna odpowiedzialna za tworzenie różnych typów rymowaczy.
+ * Implementuje interfejs Rhymersfactory.
+ */
 public class DefaultRhymersFactory implements Rhymersfactory {
 
+    /**
+     * Tworzy i zwraca standardowego rymowacza.
+     *
+     * @return instancja klasy DefaultCountingOutRhymer reprezentująca standardowego rymowacza
+     */
     @Override
     public DefaultCountingOutRhymer GetStandardRhymer() {
         return new DefaultCountingOutRhymer();
     }
 
+    /**
+     * Tworzy i zwraca "fałszywego" rymowacza, identycznego jak standardowy.
+     * Może być używany do testów lub demonstracji polimorfizmu.
+     *
+     * @return instancja klasy DefaultCountingOutRhymer
+     */
     @Override
     public DefaultCountingOutRhymer GetFalseRhymer() {
         return new DefaultCountingOutRhymer();
     }
 
+    /**
+     * Tworzy i zwraca rymowacza FIFO (First In, First Out).
+     *
+     * @return instancja klasy FIFORhymer
+     */
     @Override
     public DefaultCountingOutRhymer GetFIFORhymer() {
         return new FIFORhymer();
     }
 
+    /**
+     * Tworzy i zwraca rymowacza Hanoi, który odrzuca liczby
+     * zgodnie ze strategią inspirowaną Wieżą Hanoi.
+     *
+     * @return instancja klasy HanoiRhymer
+     */
     @Override
     public DefaultCountingOutRhymer GetHanoiRhymer() {
         return new HanoiRhymer();
